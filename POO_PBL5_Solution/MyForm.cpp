ce78bcb2b7b8f387349b62350d1c26858803cff5
Florentms <florent.maillardsalin@viacesi.fr>
2023-11-27 11:06:46 +0100

    Prosit complet

--- POO_PBL5_Solution/MyForm.cpp
+++ POO_PBL5_Solution/MyForm.cpp
@@ -9,6 +9,8 @@ int __clrcall WinMain(array<String^>^ args)
 {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
+    //Nom de projet::NomForm
     POO_PBL5_Solution::MyForm monFormulaire;
+    //Lancer l'interface
     Application::Run(% monFormulaire);
 }
